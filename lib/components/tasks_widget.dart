d7f93eb081d1f418f517aabfbdc6ca443531e20a
bath0ry <plylegacy@gmail.com>
2022-12-02 22:37:01 -0300

    add const modifier

--- lib/components/tasks_widget.dart
+++ lib/components/tasks_widget.dart
@@ -30,107 +30,105 @@ class _TasksState extends State<Tasks> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: const Color.fromARGB(255, 11, 6, 92)),
+            height: 140,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(4),
-                  color: Color.fromARGB(255, 11, 6, 92)),
-              height: 140,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: Color.fromARGB(255, 135, 115, 226),
-                  ),
-                  height: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          color: Colors.black12,
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(4),
-                          child: Image.asset(
-                            widget.foto,
-                            height: 100,
-                            width: 72,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            width: 200,
-                            child: Text(
-                              widget.nome,
-                              style: const TextStyle(
-                                  fontSize: 24,
-                                  overflow: TextOverflow.ellipsis,
-                                  fontWeight: FontWeight.w600,
-                                  color: Colors.white),
-                            ),
-                          ),
-                          Difficulty(widget.dificuldade),
-                          // vai ser um novo componente.
-                        ],
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: IconButton(
-                              onPressed: () {
-                                levelUp();
-                              },
-                              icon: Icon(Icons.arrow_upward)))
-                    ],
-                  ),
+                  color: const Color.fromARGB(255, 135, 115, 226),
                 ),
-                Row(
+                height: 100,
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        left: 8,
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.black12,
                       ),
-                      child: Container(
-                        width: 200,
-                        height: 6,
-                        child: LinearProgressIndicator(
-                          color: Colors.white,
-                          value: widget.dificuldade >= 0
-                              ? ((level / widget.dificuldade) / 10)
-                              : 1,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4),
+                        child: Image.asset(
+                          widget.foto,
+                          height: 100,
+                          width: 72,
+                          fit: BoxFit.cover,
                         ),
                       ),
                     ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(
+                          width: 200,
+                          child: Text(
+                            widget.nome,
+                            style: const TextStyle(
+                                fontSize: 24,
+                                overflow: TextOverflow.ellipsis,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.white),
+                          ),
+                        ),
+                        Difficulty(widget.dificuldade),
+                        // vai ser um novo componente.
+                      ],
+                    ),
                     Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        'Maestria: $level',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                            fontStyle: FontStyle.italic),
+                        padding: const EdgeInsets.all(16.0),
+                        child: IconButton(
+                            onPressed: () {
+                              levelUp();
+                            },
+                            icon: Icon(Icons.arrow_upward)))
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 8,
+                    ),
+                    child: Container(
+                      width: 200,
+                      height: 6,
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        value: widget.dificuldade >= 0
+                            ? ((level / widget.dificuldade) / 10)
+                            : 1,
                       ),
                     ),
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Text(
+                      'Maestria: $level',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          fontStyle: FontStyle.italic),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
